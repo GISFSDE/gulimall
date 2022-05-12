7d7c537070ac11b5c6317e7db88a5989f41a4e2b
MrDemonlxl <714416426@qq.com>
2022-05-12 13:57:53 +0800

    + GateWay

---
+++ gulimall-gateway/src/main/java/com/atguigu/mall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.mall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+/**1、开启服务注册与发现
+ * （配置nacos的注册中心的地址）*/
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
